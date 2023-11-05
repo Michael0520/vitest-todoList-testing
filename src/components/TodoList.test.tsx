aa1a7db9af4f5cb8cd2b226bde2f06e8af84a9bf
Michael0520 <po2319987@gmail.com>
2023-11-06 05:51:24 +0800

    test: 💍 add totList testing

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -1,4 +1,5 @@
 import { render, fireEvent, screen } from '@testing-library/react'
+
 import TodoList from './TodoList'
 
 describe('<TodoList />', () => {
@@ -13,28 +14,55 @@ describe('<TodoList />', () => {
     render(<TodoList />)
     const input = screen.getByPlaceholderText(/add a new task.../i)
     const addButton = screen.getByRole('button', { name: /add/i })
-
-    // Simulate user typing a new todo item and submitting it
     fireEvent.change(input, { target: { value: 'New Todo' } })
     fireEvent.click(addButton)
-
-    // The new todo item should be added to the list
     expect(screen.getByText('New Todo')).toBeInTheDocument()
   })
 
   it('allows users to delete items from the list', () => {
-    // First, add an item to the list
+    render(<TodoList />)
+    const input = screen.getByPlaceholderText(/add a new task.../i)
+    const addButton = screen.getByRole('button', { name: /add/i })
+    fireEvent.change(input, { target: { value: 'New Todo' } })
+    fireEvent.click(addButton)
+    const deleteButton = screen.getByRole('button', { name: /delete/i })
+    fireEvent.click(deleteButton)
+    expect(screen.queryByText('New Todo')).not.toBeInTheDocument()
+  })
+
+  it('allows users to edit items in the list', async () => {
     render(<TodoList />)
     const input = screen.getByPlaceholderText(/add a new task.../i)
     const addButton = screen.getByRole('button', { name: /add/i })
     fireEvent.change(input, { target: { value: 'New Todo' } })
     fireEvent.click(addButton)
 
-    // Now delete the item
-    const deleteButton = screen.getByRole('button', { name: /delete/i })
-    fireEvent.click(deleteButton)
+    const editButton = screen.getByRole('button', { name: /edit/i })
+    fireEvent.click(editButton)
 
-    // The item should be removed from the list
-    expect(screen.queryByText('New Todo')).not.toBeInTheDocument()
+    const editInput = screen.getByDisplayValue('New Todo')
+    fireEvent.change(editInput, { target: { value: 'Edited Todo' } })
+    fireEvent.keyPress(editInput, { key: 'Enter', code: 'Enter' })
+
+    const saveButton = screen.getByRole('button', { name: /save/i })
+    fireEvent.click(saveButton)
+
+    const editedTodo = await screen.findByText('Edited Todo')
+    expect(editedTodo).toBeInTheDocument()
+  })
+
+  it('allows users to save edited items in the list', () => {
+    render(<TodoList />)
+    const input = screen.getByPlaceholderText(/add a new task.../i)
+    const addButton = screen.getByRole('button', { name: /add/i })
+    fireEvent.change(input, { target: { value: 'New Todo' } })
+    fireEvent.click(addButton)
+    const editButton = screen.getByRole('button', { name: /edit/i })
+    fireEvent.click(editButton)
+    const editInput = screen.getByDisplayValue('New Todo')
+    fireEvent.change(editInput, { target: { value: 'Edited Todo' } })
+    const saveButton = screen.getByRole('button', { name: /save/i })
+    fireEvent.click(saveButton)
+    expect(screen.getByText('Edited Todo')).toBeInTheDocument()
   })
 })
