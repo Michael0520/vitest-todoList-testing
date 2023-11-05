40599b4094fc5210e3ddf78e0f0fd68f3186f13f
Michael0520 <po2319987@gmail.com>
2023-11-06 05:01:48 +0800

    style: 💄 update app ui layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,13 @@ import TodoList from './TodoList'
 
 const App: React.FC = () => {
   return (
-    <div className="flex min-h-screen justify-center bg-gray-900">
-      <TodoList />
+    <div className="flex min-h-screen flex-col items-center bg-gradient-to-br from-pink-800 via-purple-800 to-indigo-800 p-4">
+      <h1 className="mb-4 mt-8 text-4xl font-bold text-white shadow-lg">
+        Todo List
+      </h1>
+      <div className="w-full max-w-3xl rounded-lg bg-white/30 p-6 shadow-xl backdrop-blur-md">
+        <TodoList />
+      </div>
     </div>
   )
 }
